94ffd3912115d0ac19d409182d4a656b86eec1dd
Sarah-Hashmi-Codes <114784014+Sarah-Hashmi-Codes@users.noreply.github.com>
2023-01-17 18:10:02 +1100

    added categories

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+CATEGORIES =(
+        ("Health", "Health"),
+        ('Disaster Relief','Disaster Releif'), 
+        ('Education',"Education")
+    )
+    
+class Project(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    goal=models.IntegerField() #use float for money
+    image=models.URLField()
+    is_open=models.BooleanField()
+    date_created=models.DateTimeField(auto_now_add=True) #whenever project is created date will be recorded 
+    owner=models.CharField(max_length=200) #we have to change it to FK in future so he can be able to start a project
+    category = models.CharField(max_length=200, null=True, choices= CATEGORIES )
+
+class Pledge(models.Model):
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,         #when project is cancelled all pledges will be cancelled too.
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
+
---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Project,Pledge, CATEGORIES
+
+
+class PledgeSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    amount = serializers.DecimalField(max_digits=20, decimal_places=2)
+    comment = serializers.CharField(max_length=200)
+    anonymous = serializers.BooleanField()
+    supporter = serializers.CharField(max_length=200)
+    project_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
+
+# class CategorySerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField(max_length=None)
+    goal = serializers.IntegerField()
+    image = serializers.URLField()
+    is_open = serializers.BooleanField()
+    date_created = serializers.DateTimeField(read_only=True)
+    owner = serializers.CharField(max_length=200)
+    category = serializers.ChoiceField(CATEGORIES)
+    
+
+    def create(self,validated_data):    #method to create instance
+        return Project.objects.create(**validated_data)
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
